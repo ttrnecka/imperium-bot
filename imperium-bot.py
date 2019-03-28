b0ab682e5421026a90d042f679fbb607cd719850
Tomas Trnecka <trnecka@gmail.com>
2019-03-28 10:38:13 +0000

    bot reorganization

--- imperium-bot.py
+++ imperium-bot.py
@@ -31,96 +31,12 @@ rarityorder={"Common":100, "Rare":10, "Epic":5, "Legendary":1}
 async def on_message(message):
     # we do not want the bot to reply to itself
     logger.info(f"{message.author}: {message.content}")
-    cmd = message.content.lower()
-    args = cmd.split()
 
     if message.author == client.user:
         return
-    # admin commands
-    if message.content.startswith('!admin'):
-        if not is_private_admin_channel(message.channel):
-            emsg="Insuficient rights"
-            logger.error(emsg)
-            await client.send_message(message.channel, emsg)
-            return
-        if message.content.startswith('!adminlist'):
-            if len(args)==1:
-                emsg="Username missing"
-                await client.send_message(message.channel, emsg)
-                return
-            coaches = Coach.find_by_name(args[1])
-            msg = LongMessage(client,message.channel)
-            if len(coaches)==0:
-                msg.add("No coaches found")
-            for coach in coaches:
-                msg.add(f"Coach **{coach.name}**\n")
-                msg.add(f"**Bank:** {coach.account.cash} coins\n")
-                msg.add("**Collection**:")
-                msg.add("-" * 65 + "")
-                msg.add(f"{format_pack(coach.collection_with_count())}")
-                msg.add("-" * 65 + "\n")
 
-            await msg.send()
-
-    # list commands
-    if message.content.startswith('!list'):
-        coach = Coach.load_coach(str(message.author))
-        order = False if "bydate" in message.content else True
-
-        msg = LongMessage(client,message.author)
-        msg.add(f"**Bank:** {coach.account.cash} coins\n")
-        msg.add("**Collection**:\n")
-        msg.add("-" * 65 + "")
-        msg.add(f"{format_pack(coach.collection_with_count(),order)}")
-        msg.add("-" * 65 + "\n")
-        await msg.send()
-        await client.send_message(message.channel, "Collection sent to PM")
-
-    # genpack commands
-    if message.content.startswith('!genpack'):
-        if check_gen_command(cmd):
-            ptype = args[1]
-            if ptype=="player":
-                team = args[2]
-                pack = imperiumsheet.Pack(ptype,team = team)
-                pack.generate()
-            elif ptype=="training":
-                pack = imperiumsheet.Pack(ptype)
-                pack.generate()
-            elif ptype=="booster":
-                ptype = "booster_budget" if len(args)<3 else f"booster_{args[2]}"
-                pack = imperiumsheet.Pack(ptype)
-                pack.generate()
-
-            # add error handling eventually
-            coach = Coach.load_coach(str(message.author))
-            t = Transaction(pack.description(),pack.price)
-            coach.account.make_transaction(t)
-            if t.confirmed:
-                coach.add_to_collection(pack.cards)
-                coach.store_coach()
-                unp = Unpacker(client,message.channel)
-                unp.pre_message.add(f"**{pack.description()}** for **{message.author}** - **{pack.price}** coins:\n")
-                unp.pre_message.add(":gift:\n----\n")
-                unp.post_message.add("----\n:tada:\n")
-                unp.post_message.add(f"Remaining coins: **{coach.account.cash}**")
-                unp.cards_messages.extend(format_pack(pack.cards).strip("\n").split("\n"))
-                await unp.send()
-                #msg = LongMessage(client,message.channel)
-                #msg.add(f"**{pack.description()}** for **{message.author}** - **{pack.price}** coins:\n")
-                #msg.add(f"{format_pack(pack.cards)}\n")
-                #msg.add(f"Remaining coins: **{coach.account.cash}**")
-                #await msg.send()
-                # export
-                imperiumsheet.store_all_cards()
-            else:
-                emsg = "Transaction could not complete"
-                msg = LongMessage(client,message.channel)
-                msg.add(emsg)
-                logger.error(emsg)
-                await msg.send()
-        else:
-            await client.send_message(message.channel, gen_help())
+    dc = DiscordCommand(message,client)
+    await dc.process()
 
 @client.event
 async def on_ready():
@@ -129,84 +45,6 @@ async def on_ready():
     logger.info(client.user.id)
     logger.info('------')
 
-def format_pack(pack,is_sorted=True):
-    if is_sorted:
-        pack = sorted(pack, key=lambda x: (rarityorder[x["Rarity"]],x["Card Name"]))
-    msg = ""
-    for card in pack:
-        if imperiumsheet.QTY in card:
-            for c in str(card[imperiumsheet.QTY]):
-                msg+=number_emoji(c)
-            msg+=" x "
-        msg+=rarity_emoji(card["Rarity"])
-        msg+=f' **{card["Card Name"]}** ({card["Rarity"]} {card["Race"]} {card["Type"]} Card)\n'
-    return msg
-
-def gen_help():
-    msg="```"
-    msg+="USAGE:\n"
-    msg+="!genpack <type> [mixed_team] [quality]\n"
-    msg+="\t<type>:\n"
-    msg+="\t\tplayer\n"
-    msg+="\t\ttraining\n"
-    msg+="\t\tbooster\n"
-    msg+="\t[mixed_team]: (player type only)\n"
-    for key, name in imperiumsheet.MIXED_TEAMS.items():
-        msg+=f"\t\t{key} - {name}\n"
-    msg+="\t[quality]: (booster type only)\n"
-    msg+="\t\tbudget (default)\n"
-    msg+="\t\tpremium\n"
-    msg+="```"
-    return msg
-
-def is_private_admin_channel(dchannel):
-    if "admin-channel" in dchannel.name:
-        return True
-    return False
-
-def check_gen_command(command):
-    args = command.split()
-    length = len(args)
-    if length not in [2,3]:
-        return False
-
-    if args[1] not in GEN_PACKS:
-        return False
-    # training/booster without quality
-    if length == 2 and args[1] not in ["training","booster"]:
-        return False
-    # booster with allowed quality
-    if length == 3 and args[1]=="booster" and args[2] not in GEN_QUALITY:
-        return False
-    # player with teams
-    if length == 3 and args[1]=="player" and args[2] not in imperiumsheet.MIXED_TEAMS.keys():
-        return False
-    return True
-
-def rarity_emoji(rarity):
-    switcher = {
-        "Common": "",
-        "Rare": ":diamonds:",
-        "Epic": ":large_blue_diamond:",
-        "Legendary": ":large_orange_diamond: ",
-    }
-    return switcher.get(rarity, "")
-
-def number_emoji(number):
-    switcher = {
-        "0": ":zero:",
-        "1": ":one:",
-        "2": ":two:",
-        "3": ":three:",
-        "4": ":four:",
-        "5": ":five:",
-        "6": ":six:",
-        "7": ":seven:",
-        "8": ":eight:",
-        "9": ":nine:",
-
-    }
-    return switcher.get(number, "")
 
 class LongMessage:
     def __init__(self,client,channel):
@@ -238,26 +76,188 @@ class LongMessage:
                 msg += lines.pop(0) + "\n"
             yield msg
 
-class Unpacker:
-    def __init__(self,client,channel):
-        self.pre_message = LongMessage(client,channel)
-        self.post_message = LongMessage(client,channel)
-        self.cards_messages = []
-        self.client = client
-        self.channel = channel
+class DiscordCommand:
+    @classmethod
+    def is_private_admin_channel(cls,dchannel):
+        if "admin-channel" in dchannel.name:
+            return True
+        return False
 
-    async def send(self):
-        await self.pre_message.send()
-        await self.reveal_cards()
-        await self.post_message.send()
-    
-    async def reveal_cards(self):
-        for card_message in self.cards_messages:
-            secs = 3
-            message = await self.client.send_message(self.channel, f"Revealing card in {secs}s")
-            for i in range(secs-1,0,-1):
-                time.sleep(1)
-                await self.client.edit_message(message, f"Revealing card in {i}s")
-            await self.client.edit_message(message, card_message)
+    @classmethod
+    def format_pack(cls,pack,is_sorted=True):
+        if is_sorted:
+            pack = sorted(pack, key=lambda x: (rarityorder[x["Rarity"]],x["Card Name"]))
+        msg = ""
+        for card in pack:
+            if imperiumsheet.QTY in card:
+                for c in str(card[imperiumsheet.QTY]):
+                    msg+=cls.number_emoji(c)
+                msg+=" x "
+            msg+=cls.rarity_emoji(card["Rarity"])
+            msg+=f' **{card["Card Name"]}** ({card["Rarity"]} {card["Race"]} {card["Type"]} Card)\n'
+        return msg
+
+    @classmethod
+    def gen_help(cls):
+        msg="```"
+        msg+="USAGE:\n"
+        msg+="!genpack <type> [mixed_team] [quality]\n"
+        msg+="\t<type>:\n"
+        msg+="\t\tplayer\n"
+        msg+="\t\ttraining\n"
+        msg+="\t\tbooster\n"
+        msg+="\t[mixed_team]: (player type only)\n"
+        for key, name in imperiumsheet.MIXED_TEAMS.items():
+            msg+=f"\t\t{key} - {name}\n"
+        msg+="\t[quality]: (booster type only)\n"
+        msg+="\t\tbudget (default)\n"
+        msg+="\t\tpremium\n"
+        msg+="```"
+        return msg
+
+    @classmethod
+    def check_gen_command(cls,command):
+        args = command.split()
+        length = len(args)
+        if length not in [2,3]:
+            return False
+
+        if args[1] not in GEN_PACKS:
+            return False
+        # training/booster without quality
+        if length == 2 and args[1] not in ["training","booster"]:
+            return False
+        # booster with allowed quality
+        if length == 3 and args[1]=="booster" and args[2] not in GEN_QUALITY:
+            return False
+        # player with teams
+        if length == 3 and args[1]=="player" and args[2] not in imperiumsheet.MIXED_TEAMS.keys():
+            return False
+        return True
+
+    @classmethod
+    def rarity_emoji(cls,rarity):
+        switcher = {
+            "Common": "",
+            "Rare": ":diamonds:",
+            "Epic": ":large_blue_diamond:",
+            "Legendary": ":large_orange_diamond: ",
+        }
+        return switcher.get(rarity, "")
+
+    @classmethod
+    def number_emoji(cls,number):
+        switcher = {
+            "0": ":zero:",
+            "1": ":one:",
+            "2": ":two:",
+            "3": ":three:",
+            "4": ":four:",
+            "5": ":five:",
+            "6": ":six:",
+            "7": ":seven:",
+            "8": ":eight:",
+            "9": ":nine:",
+
+        }
+        return switcher.get(number, "")
+
+    def __init__(self,dmessage,dclient):
+        self.message = dmessage
+        self.client = dclient
+        self.cmd = dmessage.content.lower()
+        self.args = self.cmd.split(" ")
+
+    async def process(self):
+        if self.cmd.startswith('!admin'):
+            await self.__run_admin()
+        if self.cmd.startswith('!list'):
+            await self.__run_list()
+        if self.cmd.startswith('!genpack'):
+            await self.__run_genpack()
+
+    async def __run_admin(self):
+        # if not started from admin-channel
+        if not self.__class__.is_private_admin_channel(self.message.channel):
+            emsg="Insuficient rights"
+            logger.error(emsg)
+            await self.client.send_message(self.message.channel, emsg)
+            return
+
+        #adminlist cmd
+        if self.message.content.startswith('!adminlist'):
+            # require username argument
+            if len(self.args)==1:
+                emsg="Username missing"
+                await self.client.send_message(self.message.channel, emsg)
+                return
+
+            coaches = Coach.find_by_name(self.args[1])
+            msg = LongMessage(self.client,self.message.channel)
+
+            if len(coaches)==0:
+                msg.add("No coaches found")
+
+            for coach in coaches:
+                msg.add(f"Coach **{coach.name}**\n")
+                msg.add(f"**Bank:** {coach.account.cash} coins\n")
+                msg.add("**Collection**:")
+                msg.add("-" * 65 + "")
+                msg.add(f"{self.__class__.format_pack(coach.collection_with_count())}")
+                msg.add("-" * 65 + "\n")
+
+            await msg.send()
+            return
+
+    async def __run_list(self):
+        coach = Coach.load_coach(str(self.message.author))
+        order = False if "bydate" in self.message.content else True
+
+        msg = LongMessage(self.client,self.message.author)
+        msg.add(f"**Bank:** {coach.account.cash} coins\n")
+        msg.add("**Collection**:\n")
+        msg.add("-" * 65 + "")
+        msg.add(f"{self.__class__.format_pack(coach.collection_with_count(),order)}")
+        msg.add("-" * 65 + "\n")
+        await msg.send()
+        await self.client.send_message(self.message.channel, "Collection sent to PM")
+
+    async def __run_genpack(self):
+        if self.__class__.check_gen_command(self.cmd):
+            ptype = self.args[1]
+            if ptype=="player":
+                team = self.args[2]
+                pack = imperiumsheet.Pack(ptype,team = team)
+                pack.generate()
+            elif ptype=="training":
+                pack = imperiumsheet.Pack(ptype)
+                pack.generate()
+            elif ptype=="booster":
+                ptype = "booster_budget" if len(self.args)<3 else f"booster_{self.args[2]}"
+                pack = imperiumsheet.Pack(ptype)
+                pack.generate()
+
+            # add error handling eventually
+            coach = Coach.load_coach(str(self.message.author))
+            t = Transaction(pack.description(),pack.price)
+            coach.account.make_transaction(t)
+            if t.confirmed:
+                coach.add_to_collection(pack.cards)
+                coach.store_coach()
+                msg = LongMessage(self.client,self.message.channel)
+                msg.add(f"**{pack.description()}** for **{self.message.author}** - **{pack.price}** coins:\n")
+                msg.add(f"{self.__class__.format_pack(pack.cards)}\n")
+                msg.add(f"Remaining coins: **{coach.account.cash}**")
+                await msg.send()
+                # export
+                imperiumsheet.store_all_cards()
+            else:
+                emsg = "Transaction could not complete"
+                msg = LongMessage(self.client,self.message.channel)
+                msg.add(emsg)
+                logger.error(emsg)
+                await msg.send()
+        else:
+            await self.client.send_message(self.message.channel, gen_help())
 
 client.run(TOKEN)
